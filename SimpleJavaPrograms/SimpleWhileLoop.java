12b9f3c8940c27dcb60a39bd41892f0b37e18735
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-08-11 08:33:02 +0200

    Add files via upload

---
+++ SimpleJavaPrograms/SimpleWhileLoop.java
@@ -0,0 +1,10 @@
+/*Writing a simple while-loop in JAVA*/
+public class SimpleWhileLoop{
+	public static void main(String []args) {
+		int a=25;
+		while(a!=0) {
+			System.out.println("Curently a is: "+a);
+			a--;
+		}
+	}
+}
